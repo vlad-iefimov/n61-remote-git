8678fafe4ec126fdcc567f91e46189e8ee4fef61
Vladyslav Iefimov <vladislav.yefimov@gmail.com>
2020-09-02 17:24:31 -0800

    First remote

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class Relief {
+    constructor(size = 1) {
+        this.size = size;
+
+    }
+
+    toString() {
+        return "Appel illégal, méthode non implémentée";
+
+    }
+}
